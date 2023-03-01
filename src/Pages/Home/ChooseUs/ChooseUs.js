71e8f25e6302b33634f78623db5f177b528ded34
Shahriyar001 <shahriyarmahbub55@gmail.com>
2023-03-01 20:41:48 +0600

    last update

--- src/Pages/Home/ChooseUs/ChooseUs.js
+++ src/Pages/Home/ChooseUs/ChooseUs.js
@@ -15,7 +15,7 @@ const ChooseUs = () => {
                 <p className='text-xl font-semibold'>**************</p>
                 <p className='text-xl font-semibold'>Our main difference</p>
 
-                <p className='text-xl mt-10 mb-5 mx-20'> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum sint asperiores fuga saepe illum distinctio sunt! Nostrum natus nemo commodi quia consequuntur quidem soluta, error voluptas minus aliquid exercitationem, quae possimus expedita fugiat atque amet sapiente voluptatibus porro ducimus praesentium cumque dolores sint asperiores ipsa! Quod temporibus expedita voluptatibus nam id esse, fugiat molestias veritatis quam a qui ut, repellat aliquam ducimus et dolore iure libero facere fugit ex, dolor dolores dicta ratione. Laudantium, assumenda nihil! Ad quasi impedit assumenda.</p>
+                <p className='text-xl mt-10 mb-5 mx-20'> Eum sint asperiores fuga saepe illum distinctio sunt! Nostrum natus nemo commodi quia consequuntur quidem soluta, error voluptas minus aliquid exercitationem, quae possimus expedita fugiat atque amet sapiente voluptatibus porro ducimus praesentium cumque dolores sint asperiores ipsa! Quod temporibus expedita voluptatibus nam id esse, fugiat molestias veritatis quam a qui ut, repellat aliquam ducimus et dolore iure libero facere fugit ex, dolor dolores dicta ratione. Laudantium, assumenda nihil! Ad quasi impedit assumenda.</p>
             </div>
             <div className='grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mb-10'>
                 {
